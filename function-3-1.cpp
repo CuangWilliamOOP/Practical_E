8ac8593a4c5d16e0ea8c6a55fbd92f27d90c10f8
CuangWilliamOOP <100587630+CuangWilliamOOP@users.noreply.github.com>
2022-04-01 11:13:42 +0700

    Add files via upload

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -5,18 +5,19 @@ int count_negatives(int *vals,int len){
 			count += 1;
 		}
 
-	}
-	
-return count;
-
+	}	
+	return count;
 
 }
+
+
 int* collate_negatives(int *vals,int len, int num_negative){
 	int * array = new int[num_negative];
-
+	int count = 0;
 	for(int i = 0; i < len; i++){
 		if(vals[i] < 0){
-			array[i] = vals[i];
+			array[count] = vals[i];
+			count += 1;
 		}
 	}
 	return array;
