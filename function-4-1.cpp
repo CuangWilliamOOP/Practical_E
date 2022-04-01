99d96227203c8c3310fb7cb3ba5a6dcf2d46c55a
CuangWilliamOOP <100587630+CuangWilliamOOP@users.noreply.github.com>
2022-04-01 11:22:27 +0700

    Add files via upload

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -6,14 +6,10 @@ int sum_sides(int **vals,int n_rows, int n_cols){
 
 	for (int i = 0; i < n_rows; i++ ){
 		sum1 += vals[i][0];
-	}
-
-	for (int i = 0; i < n_rows; i++ ){
 		sum2 += vals[i][n_cols-1];
-		
 	}
 
-	int total_sum = sum1+ sum2;
-	return total_sum;
+
+	return sum1+sum2;
 
 }
