7abc5d04ca31eb445047249c28b0e0abe9923a79
CuangWilliamOOP <100587630+CuangWilliamOOP@users.noreply.github.com>
2022-04-01 10:34:54 +0700

    Add files via upload

--- main-4-1.cpp
+++ main-4-1.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-extern int sum_sides(int **vals,int num_rows, int num_cols);
+extern int sum_sides(int **vals,int n_rows, int n_cols);
 int main(){
- int row1[]={10,15,9};
+ 	int row1[]={10,15,9};
     int row2[]={11,5,3};
     int row3[]={8,56,1};
     int *vals[]={row1,row2,row3};
