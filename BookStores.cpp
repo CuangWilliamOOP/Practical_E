1efa505f7a53a06b4b4db2ada68348b6929ef52c
CuangWilliamOOP <100587630+CuangWilliamOOP@users.noreply.github.com>
2022-06-03 12:09:50 +0700

    Add files via upload

--- BookStores.cpp
+++ BookStores.cpp
@@ -8,7 +8,7 @@ void BookStores::sort(Dymocks** store, int n){
 
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n -1; j++){
-			if(store[i] -> get_storeID() > store[i] -> get_storeID()){
+			if(store[j] -> get_storeID() > store[j+1] -> get_storeID()){
 				*count = store[j];
 				store[j] = store[j+1];
 				store[j+1] = *count; 
